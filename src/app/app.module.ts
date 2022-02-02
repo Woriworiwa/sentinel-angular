9a706240a15acf6c39bd14ece02d2f3f811dbfd2
Omar Dalgamuni <omar@dalgamuni.me>
2022-02-02 22:45:16 +0100

    Refactoring to use material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { SatelliteImageComponent } from './satellite-image/satellite-image.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
+    AppComponent,    
     SatelliteImageComponent
   ],
   imports: [
@@ -27,6 +28,9 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     MatFormFieldModule,
     MatNativeDateModule,
     ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/sentinel.service.ts
+++ src/app/services/sentinel.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { WmsLayer, BBox, CRS_EPSG4326, MimeTypes, ApiType, } from '@sentinel-hub/sentinelhub-js';
+import { Layer } from '../models/layer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SentinelService {
-  url
+  url: string = 'https://services.sentinel-hub.com/ogc/wms';
+  instantId: string = '8fabe20c-9448-4608-b7f7-57ee3bdc785b'
   constructor() { }
+
+  fetchImage(layer: Layer, fromTime: Date, toTime: Date): string{
+    const wmslayer = this.createWmsLayer(layer);
+
+    const bbox = new BBox(CRS_EPSG4326, 14.95, 37.7, 15.05, 37.8);
+    const getMapParams = {
+      bbox: bbox,
+      fromTime: fromTime,
+      toTime: toTime,
+      width: 512,
+      height: 512,
+      format: MimeTypes.JPEG,
+    };
+
+    return wmslayer.getMapUrl(getMapParams, ApiType.WMS);
+  }
+
+  createWmsLayer(layer: Layer): WmsLayer{
+    return new WmsLayer({
+      baseUrl: `${this.url}/${this.instantId}`,
+      layerId: layer.id,
+    });
+  }
 }
